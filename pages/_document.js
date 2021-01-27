4288e318f0c03b477fcfd5bcbe7799660db7a61b
Mark Kop <marcelokopmann@gmail.com>
2021-01-26 23:15:15 -0300

    feat: add fonts

--- pages/_document.js
+++ pages/_document.js
@@ -36,6 +36,8 @@ export default class MyDocument extends Document {
           <meta property="og:image" content={db.bg}/>
           <meta property="og:title" content={db.title}/>
           <meta property="og:description" content={db.description}/>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
         </Head>
         <body>
           <Main />
