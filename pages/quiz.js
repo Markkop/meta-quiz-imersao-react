9b964fda3f5e63ad30b6a3228545ddf2273b4f27
Mark Kop <marcelokopmann@gmail.com>
2021-01-29 19:50:10 -0300

    feat: ResultWidget

--- pages/quiz.js
+++ pages/quiz.js
@@ -7,6 +7,39 @@ import QuizContainer from '../src/components/QuizContainer'
 import AlternativesForm from '../src/components/AlternativesForm'
 import Button from '../src/components/Button'
 
+function getNumberOfRightAnswers (results) {
+  return results.filter(Boolean).length
+}
+
+function DetailedResult ({ result, index }) {
+  const questionNumber = index + 1
+  const resultText = result ? 'Acertou' : 'Errou'
+  return <li>
+    {`# ${questionNumber} Resultado: ${resultText}`}
+  </li>
+}
+
+function ResultWidget ({ results }) {
+  const numberOfRightAnswers = getNumberOfRightAnswers(results)
+  const questionWord = numberOfRightAnswers === 1 ? 'pergunta' : 'perguntas'
+  return (
+    <Widget>
+      <Widget.Header>
+        Resultados:
+      </Widget.Header>
+
+      <Widget.Content>
+        <p>
+          {`Você acertou ${numberOfRightAnswers} ${questionWord}`}
+        </p>
+        <ul>
+          {results.map((result, index) => <DetailedResult result={result} index={index} key={index}/>)}
+        </ul>
+      </Widget.Content>
+    </Widget>
+  )
+}
+
 function LoadingWidget () {
   return (
     <Widget>
@@ -159,7 +192,7 @@ export default function QuizPage () {
 
         {screenState === screenStates.LOADING && <LoadingWidget />}
 
-        {screenState === screenStates.RESULT && <div>Você acertou X questões, parabéns!</div>}
+        {screenState === screenStates.RESULT && <ResultWidget results={results} />}
       </QuizContainer>
     </QuizBackground>
   )
